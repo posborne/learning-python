ab3cc2648f38651500349851401b9c78e7284bb0
Paul Osborne <paul-osborne@bethel.edu>
2009-03-12 15:10:36 -0500

    Went through the first three exercises in part5

---
+++ part5-modules/mymod.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+def countLines(filename):
+    """
+    Count the number of lines in the specified file
+    """
+    return len(file(filename).readlines())
+
+def countChars(filename):
+    """
+    Count the number of characters in the specified file
+    """
+    chars = 0
+    for line in file(filename).readlines():
+        chars += len(line)
+    return chars
+    
+def test(filename):
+    """
+    Count the lines and numbers of characters in the specified file.
+    This returns a tuple in the form (lines, characters) for the
+    file.
+    """
+    # this could be optimized to only open the file once
+    return (countLines(filename), countChars(filename))
