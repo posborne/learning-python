fbd6e00caf354c4b8086eb02050dc6aae3b3f2fd
Paul Osborne <paul-osborne@bethel.edu>
2009-03-12 13:25:06 -0500

    Revert "Implemented ex3 for p4"
    This reverts commit 2694621ae71841ceda058c344bc4e73ae38bd3bb.

--- part4-functions/p4ex3.py
+++ part4-functions/p4ex3.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
-def adder(**args):
+def adder(arg1, arg2, *args):
     """
-    Adds up the all the values for the keys in the
-    provided dictionary
+    Adds up the all parameters passed into the function. At least
+    two are are required.
     """
-    res = []
-    for key in args.keys():
-        res += args[key]
-    return res
+    sum = arg1 + arg2
+    for arg in args:
+        sum += arg
+    return sum
 
 if __name__ == '__main__':
     print "Testing Adder:"
-    print "adder(b=5, c=7):", adder(b=5, c=7)
+    print "adder(1, 2):", adder(1, 2)
+    print "adder('a', 'b'):", adder('a', 'b')
+    print "adder([1,2], [3,4]):", adder([1,2], [3,4])
+    print "adder(1.453, 3.1415926):", adder(1.453, 3.1415926)
+    print "adder(1, 2, 3, 4, 5):", adder(1,2,3,4,5)
+    print "adder('he', 'is', 'a', 'bird', 'nope'): ", adder('he', 'is', 'a', 'bird', 'nope')
