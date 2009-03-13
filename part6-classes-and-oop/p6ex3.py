efef428b8da19a1c30929c671670695d62d16c5c
Paul Osborne <paul-osborne@bethel.edu>
2009-03-12 22:09:45 -0500

    Made changes to fix up exercise 3

--- part6-classes-and-oop/p6ex3.py
+++ part6-classes-and-oop/p6ex3.py
@@ -1,47 +1,18 @@
 #!/usr/bin/env python
 
-import p6ex3
+from p6ex2 import MyList
 
-class MySubList(p6ex3.MyList):
+class MySubList(MyList):
+    calls = 0 # this is a static variable (all MySubLists)
 
     def __init__(self, l):
-        Super(self, l)
-        self.adds = 0
-        self.muls = 0
-        self.getitems = 0
-        self.lens = 0
-        self.getslices = 0
-        self.appends = 0
-        self.getattrs = 0
-        self.reprs = 0
+        MyList.__init__(self, l)
+        self.adds = 0 # this is an instance variable
 
     def __add__(self, other):
+        MySubList.calls += 1
         self.adds += 1
-        return Super.__add__(other)
+        return MyList.__add__(self, other)
 
-    def __mul__(self, time):
-        self.muls += 1
-        reutrn Super.__mul__(time)
-
-    def __getitem__(self, index):
-        self.getitems += 1
-        return Super.__getitem__(index)
-
-    def __len__(self):
-        self.lens += 1
-        return Super.__len__()
-
-    def __getslice__(self, low, high):
-        self.getslices += 1
-        return Super.__getslice__(low, high)
-
-    def append(self, node):
-        self.appends += 1
-        return Super.append(node)
-
-    def __getattr__(self, name):
-        return Super.__getattr__(name)
-
-    def __repr__(self):
-        return Super.__repr__()
-
+    def stats(self):
+        return self.calls, self.adds
