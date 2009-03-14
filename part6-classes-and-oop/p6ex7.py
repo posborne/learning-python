b35b1a13b0ed448a086653154a4789f9c750c1d7
Paul Osborne <osbpau@salesman.(none)>
2009-03-14 17:46:27 -0500

    Finished off example 7

--- part6-classes-and-oop/p6ex7.py
+++ part6-classes-and-oop/p6ex7.py
@@ -10,24 +10,33 @@ class Lunch(object):
         self.cust.placeOrder(foodName, self.employee)
 
     def result(self):
-        
+        self.cust.printFood()
 
 class Customer(object):
 
     def __init__(self):
+        self.food = None
 
     def placeOrder(self, foodName, employee):
-        
+        self.food = employee.takeOrder(foodName)
 
     def printFood(self):
-
+        print self.food
+        
 class Employee(object):
 
     def takeOrder(self, foodName):
-        
+        return Food(foodName)        
 
 class Food(object):
 
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+        return self.name
+
+if __name__ == '__main__':
+    l = Lunch()
+    l.order("Burritos")
+    l.result()
