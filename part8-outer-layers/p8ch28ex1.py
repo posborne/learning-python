303a5e63ed5cc5fb1f54815488d23810f71c6e07
Paul Osborne <paul-osborne@bethel.edu>
2009-03-18 20:33:54 -0500

    I decided not to implemented ch28 as it is outdated and involves minor changes to his code... you get the point.

---
+++ part8-outer-layers/p8ch28ex1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+############################################################
+# Faking the Web
+# Write a program that creates files with the same properties
+# as those created by the CGI scripts formletter.py and
+# FormEditor.py
+###########################################################
+
+# yeah, this one isn't going to be implemented.  In this area
+# the book shows its age with regard to frameworks.  You might
+# want to check out pylons, cherrypy, and/or django.
